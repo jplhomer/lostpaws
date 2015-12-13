c12f8cd24ac32a30196de0162f6d5af4a52d4ac1
Josh Larson <jplhomer@gmail.com>
2015-12-13 14:06:28 -0600

    Add more model details

--- app/pet/model.js
+++ app/pet/model.js
@@ -2,6 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
+    age: DS.attr('string'),
+    gender: DS.attr('string'),
+    details: DS.attr('string'),
+    breed: DS.attr('string'),
     color: DS.attr('string'),
     type: DS.attr('string'),
     tagNumber: DS.attr('string')
