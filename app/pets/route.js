62c990301f2f200c1b3ac83bec6523920575d646
Josh Larson <jplhomer@gmail.com>
2015-12-13 19:07:42 -0600

    Add pet list and implement fake data

--- app/pets/route.js
+++ app/pets/route.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return [
+            {
+                name: 'Shadow',
+                type: 'Dog',
+                color: 'Golden',
+                breed: 'Golden Retriever',
+                details: 'Last seen on University Ave'
+            },
+            {
+                name: 'Sassy',
+                type: 'Cat',
+                color: 'Calico',
+                breed: 'American House Cat',
+                details: 'Last spotted with her food on the porch'
+            },
+            {
+                name: 'Chance',
+                type: 'Dog',
+                color: 'White',
+                breed: 'American Bulldog',
+                details: 'Recently caught eating trash out of the kitchen'
+            }
+        ]
+    }
 });
